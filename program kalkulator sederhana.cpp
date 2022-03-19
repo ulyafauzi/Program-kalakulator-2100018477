de6d82552531ebf7d8cd4d08725c7d9f7a6d74d5
ulyafauzi <101854057+ulyafauzi@users.noreply.github.com>
2022-03-19 14:20:21 +0700

    Add files via upload

---
+++ program kalkulator sederhana.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  float a, b, pilih, hasil;
+  char ulang;
+
+  	awal:
+  	cout << "PROGRAM CALCULATOR SEDERHANA" << endl;
+  	cout << "\n";
+  	cout << "1.perkalian\n";
+  	cout << "2.pembagian\n";
+    cout << "3.penambahan\n";
+    cout << "4.pengurangan\n";
+
+  	cout << "pilih operasi (1,2,3,4) :";
+  	cin >> pilih;
+
+  	cout << endl;
+  
+  	if(pilih == 1){
+    cout << "masukan angka a =";
+    cin >> a;
+    cout << "masukan angka b =";
+    cin >> b;
+    hasil = a * b;
+    cout << a << "x" << b << "=" << hasil;
+    }
+
+    else if(pilih == 2){
+    cout << "masukan angka a =";
+    cin >> a;
+    cout << "masukan angka b =";
+    cin >> b;
+    hasil = a / b;
+    cout << a << ":" << b << "=" << hasil;
+    }
+
+    else if(pilih == 3){
+    cout << "masukan angka a =";
+    cin >> a;
+    cout << "masukan angka b =";
+    cin >> b;
+    hasil = a + b;
+    cout << a << "+" << b << "=" << hasil;
+    }
+   
+    else if(pilih == 4){
+    cout << "masukan angka a =";
+    cin >> a;
+    cout << "masukan angka b =";
+    cin >> b;
+    hasil = a - b;
+    cout << a << "-" << b << "=" << hasil;
+    }
+
+    else{
+    	cout << "maaf pilih sesuai operasi";
+    }
+  
+    cout << "\n\n";
+    cout << "apakah anda ingi ulang (y/t) :";
+    cin >> ulang;
+  
+    if(ulang == 'y'){
+    goto awal;
+    }
+  
+    else{
+    cout << "terima kasih anda telah keluar";
+    }
+    return 0;
+}
